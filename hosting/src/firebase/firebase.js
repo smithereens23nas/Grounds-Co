39a2ce75b72c65b342e044d82aad5214217a55e3
smiethereens23nas <nicholas23smith@gmail.com>
2023-07-17 00:03:01 -0400

    first commit of the react and firebase project

---
+++ hosting/src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCTsKEgZTW6uMFUI6FqZlMDOQj1FcECUwQ",
+    authDomain: "grounds-and-company.firebaseapp.com",
+    projectId: "grounds-and-company",
+    storageBucket: "grounds-and-company.appspot.com",
+    messagingSenderId: "64569737724",
+    appId: "1:64569737724:web:9b189181608a36ffb6f279",
+    measurementId: "G-R0H3QLL22T"
+  };
+
+  export const app = initializeApp(firebaseConfig);
